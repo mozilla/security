c226ec60c766431fcdac06804153d37f4cebe4e8
Gene Wood <gene_wood@cementhorizon.com>
2015-05-21 16:58:08 -0700

    Prepping for publication of ip2instance to pypi Adding documentation Updating to be pep8 conforming

--- ip2instance/__init__.py
+++ ip2instance/__init__.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import boto.ec2
 import boto.sts
 import logging
@@ -7,15 +11,15 @@ import datetime
 import yaml
 import os
 import os.path
-#import re
 
-#logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 
-# rfc1918 = re.compile("^(10\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|172\.(1[6-9]|2[0-9]|3[0-1])\.[0-9]{1,3}\.[0-9]{1,3}|192\.168\.[0-9]{1,3}\.[0-9]{1,3})$")
 
 def get_config():
-    config = {'rich_line_format': '# "{account}", "{region}", "{date}", "{name}", "{instance_type}", "{id}", "{account alias}", "{tags}"\n{ip_address}',
-              'role_session_name': 'OpSecSecurityAudit',
+    config = {'rich_line_format': '# "{account}", "{region}", "{date}", '
+              '"{name}", "{instance_type}", "{id}", "{account alias}", '
+              '"{tags}"\n{ip_address}',
+              'role_session_name': 'ip2instanceRoleSessionName',
               'roles': []}
     CONFIG_FILENAME = "/etc/ip2instance.yaml"
     if os.path.isfile(CONFIG_FILENAME) and os.access(CONFIG_FILENAME, os.R_OK):
@@ -23,10 +27,11 @@ def get_config():
             config.update(yaml.load(f))
     return config
 
+
 def get_instances(config, ip_address=None):
-    returned_instances=[]
-    # This constrains the permission that this tool requests to a subset of what 
-    # the assumed role could provide
+    returned_instances = []
+    # This constrains the permission that this tool requests to a subset of
+    # what the assumed role could provide
     policy = '''{
       "Version": "2012-10-17",
       "Statement": [
@@ -40,30 +45,32 @@ def get_instances(config, ip_address=None):
         }
       ]
     }'''
-    
+
     # Skip regions that require special access to use
     # https://github.com/boto/boto/issues/1951
-    special_access_regions=["cn-north-1",
-                            "us-gov-west-1"]
-    
+    special_access_regions = ["cn-north-1",
+                              "us-gov-west-1"]
+
     regions = boto.ec2.regions()
     date = datetime.datetime.now().isoformat()
-    
+
     logging.debug("Connecting to sts")
     try:
         conn_sts = boto.sts.connect_to_region('us-east-1')
     except Exception, e:
         logging.error("Unable to connect to sts with exception %s" % e)
-    
+
     assumed_roles = {}
     for role_arn in config['roles']:
         assumed_roles[role_arn] = {}
         try:
-            assumed_roles[role_arn]['credentials'] = conn_sts.assume_role(role_arn=role_arn,
-                                                        role_session_name=config['role_session_name'],
-                                                        policy=policy)
+            assumed_roles[role_arn]['credentials'] = conn_sts.assume_role(
+                role_arn=role_arn,
+                role_session_name=config['role_session_name'],
+                policy=policy)
         except Exception, e:
-            logging.error("Unable to assume role %s due to exception %s" % (role_arn, e))
+            logging.error("Unable to assume role %s due to exception %s" %
+                          (role_arn, e))
             continue
         try:
             credentials = assumed_roles[role_arn]['credentials']
@@ -72,13 +79,19 @@ def get_instances(config, ip_address=None):
                 aws_secret_access_key=credentials.credentials.secret_key,
                 security_token=credentials.credentials.session_token)
         except Exception, e:
-            logging.error("Unable to connect to iam with role %s due to exception %s" % (role_arn, e))
+            logging.error("Unable to connect to iam with role %s due to "
+                          "exception %s" % (role_arn, e))
         try:
-            aliases = assumed_roles[role_arn]['conn_iam'].get_account_alias()['list_account_aliases_response']['list_account_aliases_result']['account_aliases']
-            assumed_roles[role_arn]['alias'] = ', '.join(aliases) if len(aliases) > 0 else ''
+            aliases = (assumed_roles[role_arn]['conn_iam'].get_account_alias()
+                       ['list_account_aliases_response']
+                       ['list_account_aliases_result']['account_aliases'])
+            assumed_roles[role_arn]['alias'] = (', '.join(aliases)
+                                                if len(aliases) > 0
+                                                else '')
         except Exception, e:
-            logging.error("Unable to get account alias with role %s due to exception %s" % (role_arn, e))
-    
+            logging.error("Unable to get account alias with role %s due to "
+                          "exception %s" % (role_arn, e))
+
     for region in [x for x in regions if x.name not in special_access_regions]:
         for role_arn in config['roles']:
             try:
@@ -89,9 +102,11 @@ def get_instances(config, ip_address=None):
                     security_token=credentials.credentials.session_token,
                     region=region)
             except Exception, e:
-                logging.error("Unable to connect to aws with role %s in region %s with exception %s" % (role_arn, region, e))
+                logging.error("Unable to connect to aws with role %s in "
+                              "region %s with exception %s" %
+                              (role_arn, region, e))
                 continue
-    
+
             instances = conn_ec2.get_only_instances()
             for instance in [x for x in instances if x.state == "running"]:
                 if instance.ip_address is not None:
@@ -101,23 +116,30 @@ def get_instances(config, ip_address=None):
                         'account': role_arn.split(':')[4],
                         'region': region.name,
                         'account alias': assumed_roles[role_arn]['alias'],
-                        'name': instance.tags['Name'] if 'Name' in instance.tags else ""}
-                    if ip_address is not None and instance.ip_address == ip_address:
+                        'name': (instance.tags['Name']
+                                 if 'Name' in instance.tags
+                                 else "")}
+                    if (ip_address is not None
+                            and instance.ip_address == ip_address):
                         # short circuit as soon as we find the IP and return it
                         return instance
                     else:
                         returned_instances.append(instance)
     return returned_instances
 
+
 def print_ip_list(config, instances):
     for instance in instances:
         print instance.ip_address
 
+
 def print_rich_ip_list(config, instances):
     for instance in instances:
         extra_fields = instance.extra_fields
-        print config['rich_line_format'].format(**dict(instance.__dict__.items() + 
-                                                  extra_fields.items()))
+        print config['rich_line_format'].format(
+            **dict(instance.__dict__.items() +
+                   extra_fields.items()))
+
 
 def main():
     config = get_config()
--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
-from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from setuptools import setup, find_packages
+# Always prefer setuptools over distutils
 from codecs import open  # To use a consistent encoding
 from os import path
 
@@ -14,13 +21,15 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.0.0',
+    version='1.0.3',
 
-    description='Search through multiple AWS accounts for instance IP addresses',
+    description='Search through multiple AWS accounts for instance IP '
+                'addresses',
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/mozilla/nmap-differential-scan/tree/master/ip2instance',
+    url='https://github.com/mozilla/nmap-differential-scan'
+        '/tree/master/ip2instance',
 
     # Author details
     author='Gene Wood',
@@ -57,31 +66,16 @@ setup(
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
 
-    # List run-time dependencies here.  These will be installed by pip when your
-    # project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=[],
+    install_requires=['boto', 'PyYAML'],
 
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
     package_data={
     },
 
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages.
-    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     data_files=[],
 
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
             'ip2instance=ip2instance:main',
         ],
     },
 )
-
