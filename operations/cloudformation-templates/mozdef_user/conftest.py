186134d5245ea39f5ca87717a0465b21962147c7
Aaron Meihm <ameihm@mozilla.com>
2017-03-09 16:30:37 -0600

    add mig sqs resource to mozdef user configuration (#9)
    * add mig sqs resource to mozdef user configuration
    
    * reference mig sqs queue pr in metadata

--- operations/cloudformation-templates/mozdef_user/conftest.py
+++ operations/cloudformation-templates/mozdef_user/conftest.py
@@ -14,6 +14,7 @@ def config(request):
             "BlocklistBucketName": "mozilla_infosec_blocklist",
             "IPSpaceBucketName": "mozilla-ipspace",
             "InfosecQueueArn": "arn:aws:sqs:us-west-1:656532927350:infosec_mozdef_events",
+            "MIGQueueArn": "arn:aws:sqs:us-west-2:371522382791:mig-log-sqs",
             "NubisQueueArn": "arn:aws:sqs:us-west-1:656532927350:nubis_events_prod",
             "FxaQueueArn": "arn:aws:sqs:us-west-2:361527076523:fxa-customs-prod",
             "source_arn": "arn:aws:iam::656532927350:user/mozdef"
@@ -24,6 +25,7 @@ def config(request):
             "BlocklistBucketName": "mozilla_infosec_blocklist",
             "IPSpaceBucketName": "MISSINGmozdef-gen2-privateIssue147",
             "InfosecQueueArn": "arn:aws:sqs:us-west-1:656532927350:infosec_mozdef_events_non_prod",
+            "MIGQueueArn": "arn:aws:sqs:us-west-2:656532927350:mig-log-sqs",
             "NubisQueueArn": "arn:aws:sqs:us-west-1:656532927350:nubis_events_non_prod",
             "FxaQueueArn": "arn:aws:sqs:us-west-2:361527076523:fxa-customs-prod",
             "source_arn": "arn:aws:iam::656532927350:user/mozdef"
