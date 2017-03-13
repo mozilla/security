ee9e2d982eb088ed6453254cd179748304cce415
Gene Wood <gene_wood@cementhorizon.com>
2017-03-13 13:03:16 -0700

    Update the manage_iam_role calling out that it's no longer needed

--- operations/lambda_functions/manage_iam_role/README.md
+++ operations/lambda_functions/manage_iam_role/README.md
@@ -1,4 +1,8 @@
-Package manage_iam_role and upload to S3
+# RoleName argument in CloudFormation makes `manage_iam_role` unnecessary
+
+With the addition of the `RoleName` argument to the `AWS::IAM::Role` CloudFormation object around [August 2016](http://web.archive.org/web/20160822015322/http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-iam-role.html), this `manage_iam_role` code should no longer be necessary.
+
+# Package manage_iam_role and upload to S3
 
 dir="`mktemp --directory`"
 pip install cfnlambda --no-deps -t "$dir"
@@ -8,3 +12,12 @@ aws --profile infosec-prod --region us-west-2 s3 cp "$dir/manage_iam_role.zip" s
 aws --profile infosec-prod --region us-east-1 s3 cp "$dir/manage_iam_role.zip" s3://infosec-lambda-us-east-1/
 rm -rf "$dir"
 
+# Package for testing
+dir="`mktemp --directory`"
+# pip install cfnlambda --no-deps -t "$dir"
+cp -v /home/gene/code/github.com/gene1wood/cfnlambda/cfnlambda.py "$dir"
+cp -v manage_iam_role.py "$dir"
+zip --junk-paths $dir/manage_iam_role_dev.zip "$dir/manage_iam_role.py" "$dir/cfnlambda.py"
+aws --profile infosec-prod --region us-west-2 s3 cp "$dir/manage_iam_role_dev.zip" s3://infosec-lambda-us-west-2/
+aws --profile infosec-prod --region us-east-1 s3 cp "$dir/manage_iam_role_dev.zip" s3://infosec-lambda-us-east-1/
+rm -rf "$dir"
--- operations/lambda_functions/manage_iam_role/manage_iam_role.py
+++ operations/lambda_functions/manage_iam_role/manage_iam_role.py
@@ -1,5 +1,5 @@
 import boto3
-from cfnlambda import handler_decorator
+from cfnlambda import handler_decorator, RequestType
 import botocore.exceptions
 import logging
 import json
@@ -9,7 +9,7 @@ logging.getLogger().setLevel(logging.INFO)
 
 
 def get_assume_role_policy_document(AssumeRolePolicyDocument=None,
-                                    TrustedEntities=None):
+                                    TrustedEntities=[]):
     if AssumeRolePolicyDocument is not None:
         return AssumeRolePolicyDocument
     assume_role_policy_obj = json.loads('''{
@@ -26,45 +26,115 @@ def get_assume_role_policy_document(AssumeRolePolicyDocument=None,
         }
       ]
     }''')
-    if TrustedEntities is not None:
+    # We are assuming that we also want to permit the ec2 service to assume
+    # this role (to enable ec2 instances to have this role assigned to them).
+    # Alternatives could be lambda, other AWS services or no services.
+    aws_entities = [x for x in TrustedEntities if len(x) > 0]
+    if len(aws_entities) > 0:
         (assume_role_policy_obj['Statement'][0]
-         ['Principal']['AWS']) = TrustedEntities
-        # Hopefully this is an array and not a comma delimited list
+         ['Principal']['AWS']) = aws_entities
+        # The TrustedEntities is a "CommaDelimitedList"
     return json.dumps(assume_role_policy_obj)
 
 
 def create_iam_role(RoleName,
                     Path='/',
                     AssumeRolePolicyDocument=None,
-                    TrustedEntities=None,
+                    TrustedEntities=[],
                     **kwargs):
     iam_client = boto3.client('iam')
+
     AssumeRolePolicyDocument = get_assume_role_policy_document(
         AssumeRolePolicyDocument,
         TrustedEntities)
-    role = iam_client.create_role(
-        Path=Path,
-        RoleName=RoleName,
-        AssumeRolePolicyDocument=AssumeRolePolicyDocument)
-    return {'result': 'Role %s created or updated successfully' %
-            RoleName,
+    try:
+        role = iam_client.create_role(
+            Path=Path,
+            RoleName=RoleName,
+            AssumeRolePolicyDocument=AssumeRolePolicyDocument)
+    except botocore.exceptions.ClientError as e:
+        if 'EntityAlreadyExists' in e.message:
+            # Since we don't compare the existing AssumeRolePolicyDocument with
+            # the new one (because it's difficult to do a deep comparison
+            # ignoring list order) this update_assume_role_policy call could
+            # be unnecessary.
+            iam_client.update_assume_role_policy(
+                RoleName=RoleName,
+                PolicyDocument=AssumeRolePolicyDocument)
+            response = iam_client.get_role(RoleName=RoleName)
+            arn = response['Role']['Arn']
+            return {'result': 'Role %s already exists and was updated '
+                    'successfully' % RoleName,
+                    'Arn': arn}
+        else:
+            raise
+    return {'result': 'Role %s created successfully' % RoleName,
             'Arn': role['Role']['Arn']}
+
+
+def update_iam_role(old, new):
+    iam_client = boto3.client('iam')
+    # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/crpg-ref-requests.html
+
+    if old['RoleName'] != new['RoleName']:
+        raise ValueError(
+            'RoleName can not be updated. To update the RoleName delete %s '
+            'and then create %s' % (old['RoleName'], new['RoleName']))
+
+    response = iam_client.get_role(RoleName=new['RoleName'])
+    arn = response['Role']['Arn']
+
+    # TODO : Since the IAM Role that this lambda function creates could have
+    # been changed by something other than the CloudFormaiton stack that
+    # spawned it, we should compare the new values against the real current
+    # values instead of the old values reported by CloudFormation
+    # response['Role']['AssumeRolePolicyDocument']
+
+    if (
+        (
+            'AssumeRolePolicyDocument' in new and
+            'AssumeRolePolicyDocument' in old and
+            new['AssumeRolePolicyDocument'] == old['AssumeRolePolicyDocument']
+        ) and (
+            (
+                new['TrustedEntities'] is None and
+                old['TrustedEntities'] is None
+            ) or (
+                new['TrustedEntities'] is not None and
+                old['TrustedEntities'] is not None and
+                set(new['TrustedEntities']) == set(old['TrustedEntities'])))):
+        return {'result': 'Role %s was not updated as there were no '
+                'changes' % new['RoleName'],
+                'Arn': arn}
+
+    AssumeRolePolicyDocument = get_assume_role_policy_document(
+        AssumeRolePolicyDocument=(new['AssumeRolePolicyDocument']
+                                  if 'AssumeRolePolicyDocument' in new
+                                  else None),
+        TrustedEntities=new['TrustedEntities'])
+
+    iam_client.update_assume_role_policy(
+        RoleName=new['RoleName'],
+        PolicyDocument=AssumeRolePolicyDocument)
+
+    return {'result': 'Role %s updated successfully' % new['RoleName'],
+            'Arn': arn}
 
 
 def delete_iam_role(RoleName):
     try:
         iam_client = boto3.client('iam')
         iam_client.delete_role(RoleName=RoleName)
-    except Exception as e:
-        if (type(e) is botocore.exceptions.ClientError and
-                'NoSuchEntity' in e.message):
-            logger.info('Skipping deletion of Role %s as it does not '
-                        'exist' % RoleName)
+    except botocore.exceptions.ClientError as e:
+        if 'NoSuchEntity' in e.message:
+            logger.info('Skipping deletion of Role %s as it does not exist'
+                        % RoleName)
+            return {'result': 'Skipping deletion of Role %s as it does not '
+                    'exist' % RoleName}
         else:
             raise
 
-    return {'result': 'Role %s deleted successfully'
-            % RoleName}
+    return {'result': 'Role %s deleted successfully' % RoleName}
 
 
 @handler_decorator()
@@ -86,8 +156,11 @@ def manage_iam_role(event, context):
     if 'RoleName' not in event['ResourceProperties']:
         raise ValueError('RoleName argument not present in ResourceProperties')
 
-    if event['RequestType'] == 'Delete':
+    if event['RequestType'] == RequestType.DELETE:
         result = delete_iam_role(event['ResourceProperties']['RoleName'])
-    else:
+    elif event['RequestType'] == RequestType.UPDATE:
+        result = update_iam_role(event['OldResourceProperties'],
+                                 event['ResourceProperties'])
+    elif event['RequestType'] == RequestType.CREATE:
         result = create_iam_role(**event['ResourceProperties'])
     return result
